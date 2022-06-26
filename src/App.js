91f2d33a621cee396f3c0e4666f822535a080a66
Zack Hersh <zackshersh@gmail.com>
2022-06-26 15:49:28 -0400

    nav started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Block from "./components/Block";
+import Nav from "./components/Nav";
 import Title from "./components/Title";
 import "./main.css"
 
 function App() {
     return (
         <div className="App">
+            <Nav></Nav>
             <Block title={true} background={"Dark"}>
                 <Title />
             </Block>
+            <div style={{padding: "24px", height: "200vh"}}></div>
         </div>
     );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../title.css"
+import Nav from './Nav';
 
 function Title(props) {
     return (
@@ -7,6 +8,9 @@ function Title(props) {
             <h3>Welcome To</h3>
             <h1>Behavioral <br/> Health <br/> Solutions</h1>
             <p>Behavioral Health Solutions is a team of experienced professionals who provide caring and effective psychotherapy to adolescents and adults</p>
+            <div className='Nav-Container'>
+                <Nav></Nav>
+            </div>
         </div>
     );
 }
