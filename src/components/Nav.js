c91fa086d104ade9c86d00f36f492863e35f1c44
Zack Hersh <zackshersh@gmail.com>
2022-06-26 15:38:18 -0400

    basic title styling

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Nav(props) {
+    return (
+        <nav>
+            <button>Staff</button>
+            <button>Services</button>
+        </nav>
+    );
+}
+
+export default Nav;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "../title.css"
 
 function Title(props) {
     return (
-        <div>
+        <div className='Title'>
             <h3>Welcome To</h3>
             <h1>Behavioral <br/> Health <br/> Solutions</h1>
             <p>Behavioral Health Solutions is a team of experienced professionals who provide caring and effective psychotherapy to adolescents and adults</p>
